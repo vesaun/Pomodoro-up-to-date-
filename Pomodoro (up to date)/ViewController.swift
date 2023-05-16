f62ea9681089ece0d31cad75970093455ee2106e
nthnns <naso7797@colorado.edu>
2023-05-16 17:10:53 -0600

    Fixed play/pause button 5/16/23

--- Pomodoro (up to date)/ViewController.swift
+++ Pomodoro (up to date)/ViewController.swift
@@ -13,26 +13,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-    @IBOutlet weak var play: UIButton!
-    
     @IBOutlet weak var timer_display: UILabel!
-    var secondsRemaining = 10*60 - 1
     
-    @IBAction func buttonPressed(_ sender: Any)
+    @IBOutlet weak var button: UIButton!
+    var time_remaining =  10 * 60
+    var timer = Timer()
+    var isPaused = true
+    
+    func startTimer()
     {
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if self.secondsRemaining > 0 {
-                 
-                let _minutes = String(self.secondsRemaining / 60)
-                let _seconds = String(self.secondsRemaining % 60)
-                self.secondsRemaining -= 1
-                self.timer_display.text = _minutes + ":" + _seconds
-            } else {
-                Timer.invalidate()
-            }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
+        
+    }
+    
+    func endTimer()
+    {
+        timer.invalidate()
+    }
+    
+    @objc func countdown()
+    {
+        if time_remaining > 0
+        {
+            time_remaining -= 1
+        }
+        else
+        {
+            endTimer()
+        }
+            let _minutes = String(time_remaining / 60)
+            let _seconds = String(time_remaining % 60)
+            timer_display.text = _minutes + ":" + _seconds
+        
+        
+    }
+    
+    @IBAction func playPause(_ sender: Any)
+    {
+        if isPaused == true
+        {
+            startTimer()
+            isPaused = false
+        }
+        else
+        {
+            endTimer()
+            isPaused = true
         }
     }
+    
+    
     
     }
     
