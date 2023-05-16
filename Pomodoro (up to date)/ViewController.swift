9dc540fe6bc61a1e5c36b6fe3496e285ed4aaa99
nthnns <naso7797@colorado.edu>
2023-05-16 14:39:15 -0600

    nathan's timer 05/16/23 - pause button needs to be fixed. Ready to push.

--- Pomodoro (up to date)/ViewController.swift
+++ Pomodoro (up to date)/ViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
+    @IBOutlet weak var play: UIButton!
+    
+    @IBOutlet weak var timer_display: UILabel!
+    var secondsRemaining = 10*60 - 1
+    
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                 
+                let _minutes = String(self.secondsRemaining / 60)
+                let _seconds = String(self.secondsRemaining % 60)
+                self.secondsRemaining -= 1
+                self.timer_display.text = _minutes + ":" + _seconds
+            } else {
+                Timer.invalidate()
+            }
+        }
+    }
+    
+    }
+    
 
+    
 
-}
 
